d19664dffc55e83ade4ac5810f8cffdcc6389efb
Rinat <astrakhan.rinat@gmail.com>
2019-03-11 11:49:10 +0400

    Create What's for Diner.js

---
+++ Intro to JS: Drawing & Animation/What's for Diner.js
@@ -0,0 +1,38 @@
+background(199, 150, 16); // wooden table
+ellipse(200, 200, 350, 350); // plate
+fill(208, 228, 245);
+ellipse(200, 200, 300, 300); 
+
+// scrambled egg
+noStroke();
+fill(255, 255, 255);
+beginShape();
+vertex(232,116);
+bezierVertex(284,128,330,234,207,276);
+bezierVertex(30,304,79,83,232,116);
+endShape();
+fill(255, 191, 0);
+ellipse(180, 190, 80, 80);
+
+// peas
+fill(104, 186, 37);
+ellipse(100, 126, 20, 20);
+ellipse(134, 100, 20, 20);
+ellipse(123, 135, 20, 20);
+ellipse(157, 119, 20, 20);
+ellipse(202, 111, 20, 20);
+ellipse(161, 94, 20, 20);
+ellipse(180, 110, 20, 20);
+ellipse(192, 90, 20, 20);
+ellipse(132, 76, 20, 20);
+ellipse(156, 71, 20, 20);
+ellipse(178, 71, 20, 20);
+ellipse(99, 150, 20, 20);
+ellipse(146, 142, 20, 20);
+ellipse(110, 99, 20, 20);
+
+// sausages
+stroke(250, 85, 85);
+strokeWeight(23);
+line(297, 230, 159, 293);
+line(324, 275, 163, 328);
