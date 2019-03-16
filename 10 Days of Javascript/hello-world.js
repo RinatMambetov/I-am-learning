68322b7e800f98526527891c82a7ba99d7a9d92f
Rinat <astrakhan.rinat@gmail.com>
2019-03-16 05:33:35 +0300

    Update hello-world.js

--- 10 Days of Javascript/hello-world.js
+++ 10 Days of Javascript/hello-world.js
@@ -1,3 +1,5 @@
+
+
 /**
 *   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
 *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
@@ -8,7 +10,8 @@
 function greeting(parameterVariable) {
     // This line prints 'Hello, World!' to the console:
     console.log('Hello, World!');
-
+    console.log(parameterVariable);
     // Write a line of code that prints parameterVariable to stdout using console.log:
     
 }
+
