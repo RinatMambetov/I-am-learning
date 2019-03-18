f6bc41f079b0a0304cc97f983c54a1e0d0b386cd
Rinat <astrakhan.rinat@gmail.com>
2019-03-18 05:36:30 +0300

    Create create -a-rectangle-object.js

---
+++ 10 Days of Javascript/create -a-rectangle-object.js
@@ -0,0 +1,15 @@
+
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    let rect = {
+        length: a,
+        width: b,
+        perimeter: 2 * (a + b),
+        area: a * b
+    };
+    return rect;
+}
+
