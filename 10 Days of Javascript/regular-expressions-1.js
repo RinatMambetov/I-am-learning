d7ebcb4b5f60e4974e5dfa0ab4136b613d9df225
Rinat <astrakhan.rinat@gmail.com>
2019-03-19 05:57:53 +0300

    Create regular-expressions-1.js

---
+++ 10 Days of Javascript/regular-expressions-1.js
@@ -0,0 +1,16 @@
+
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+    // const re = /^[aeiou]\w*[aeiou]$/;
+    const re = /(^a\w*a$)|(^e\w*e$)|(^i\w*i$)|(^o\w*o$)|(^u\w*u$)/;
+
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
