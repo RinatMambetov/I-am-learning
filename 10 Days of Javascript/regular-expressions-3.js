4d8dcdee35cd59b2019595f64dfdd35d9c96c616
Rinat <astrakhan.rinat@gmail.com>
2019-03-19 05:59:40 +0300

    Create regular-expressions-3.js

---
+++ 10 Days of Javascript/regular-expressions-3.js
@@ -0,0 +1,14 @@
+
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    let re = /\d+/g;
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
