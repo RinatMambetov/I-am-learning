78388def75cd9f39a4508d9466bb05f0709c02fa
Rinat <astrakhan.rinat@gmail.com>
2019-03-19 06:05:08 +0300

    Create buttonGrid.js

---
+++ 10 Days of Javascript/buttons-container/js/buttonGrid.js
@@ -0,0 +1,21 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+
+btn5.addEventListener ("click", function() {
+    let btn4Value = btn4.innerHTML;
+    btn4.innerHTML = btn7.innerHTML;
+    btn7.innerHTML = btn8.innerHTML;
+    btn8.innerHTML = btn9.innerHTML;
+    btn9.innerHTML = btn6.innerHTML;
+    btn6.innerHTML = btn3.innerHTML;
+    btn3.innerHTML = btn2.innerHTML;
+    btn2.innerHTML = btn1.innerHTML;
+    btn1.innerHTML = btn4Value;
+});
