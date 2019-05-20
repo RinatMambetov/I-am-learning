58333a1780d813e83e57462950727e4dc8de6857
Rinat Mambetov <astrakhan.rinat@gmail.com>
2019-05-20 13:45:11 +0400

    add eslint

--- 10-days-of-JS/arrays.js
+++ 10-days-of-JS/arrays.js
@@ -1,17 +1,16 @@
-
-
 /**
 *   Return the second largest number in the array.
 *   @param {Number[]} nums - An array of numbers.
 *   @return {Number} The second largest number in the array.
-**/
+* */
 function getSecondLargest(nums) {
-    // Complete the function
-    nums = nums.sort( (x, y) => x > y );
-    let max = nums[nums.length - 1];
-    do {
-        nums.pop();
-    } while (nums[nums.length - 1] === max);
-    return nums[nums.length - 1];
+  // Complete the function
+  const newNums = nums.sort((x, y) => x > y);
+  const max = newNums[newNums.length - 1];
+  do {
+    newNums.pop();
+  } while (newNums[newNums.length - 1] === max);
+  return newNums[newNums.length - 1];
 }
 
+console.log(getSecondLargest(1, 3, 2));
